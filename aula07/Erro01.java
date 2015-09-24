b6aa433647daceb0d6c89cc86c0382254726c6be
Bruno Rodrigues Silva <brsilva.eng@gmail.com>
2015-09-24 20:02:23 -0300

    aula 07
    diferentes erros de sintaxe e semântica

---
+++ aula07/Erro01.java
@@ -0,0 +1,8 @@
+class Erro01 {
+
+	public static void main (String[] args) {
+
+		System.out.println("Hello World !");
+
+	}
+}
---
+++ aula07/Erro03.java
@@ -0,0 +1,8 @@
+class Erro03 {
+
+	public static void main (String[] args) {
+
+		System.out.println("você digitou " + 1/0);
+
+	}
+}
